d967e4a02d6f9db294ac0a2d7a1c6228949f0c1d
Alexis <xavieralexisariashuapaya@hotmail.com>
2024-11-16 14:41:49 -0500

    feat: Aside layout

--- src/app/main/layouts/aside-layout/aside-layout.component.ts
+++ src/app/main/layouts/aside-layout/aside-layout.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEllipsis, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-aside-layout',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, MatBadgeModule],
   templateUrl: './aside-layout.component.html',
   styleUrl: './aside-layout.component.css'
 })
 export class AsideLayoutComponent {
-
+  faSearch = faSearch;
+  faEllipsis = faEllipsis;
 }
