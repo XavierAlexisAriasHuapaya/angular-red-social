d585ad3ce2b8f6db5b8711f8661c4bf6aec346b3
Alexis <xavieralexisariashuapaya@hotmail.com>
2024-11-16 18:05:00 -0500

    feat: log out

--- src/app/main/layouts/header-layout/header-layout.component.ts
+++ src/app/main/layouts/header-layout/header-layout.component.ts
@@ -1,30 +1,40 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBell, faGear, faHome, faMessage, faPeopleGroup, faShop, faUser, faUserGroup, faVideo } from '@fortawesome/free-solid-svg-icons';
+import { MatMenuModule } from '@angular/material/menu';
+import { faBell, faClose, faGear, faHome, faMessage, faPeopleGroup, faRightFromBracket, faRightToBracket, faShop, faUser, faUserGroup, faVideo } from '@fortawesome/free-solid-svg-icons';
+import { AuthenticationService } from '../../../authentication/services/authentication.service';
 
 @Component({
   selector: 'app-header-layout',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, MatMenuModule],
   templateUrl: './header-layout.component.html',
   styleUrl: './header-layout.component.css'
 })
 export class HeaderLayoutComponent {
-  faHome = faHome;
-  faUserGroup = faUserGroup;
-  faVideo = faVideo;
-  faShop = faShop;
-  faPeopleGroup = faPeopleGroup;
-  faConfig = faGear;
-  faMessage = faMessage;
-  faBell = faBell;
-  faUser = faUser;
+  public faHome = faHome;
+  public faUserGroup = faUserGroup;
+  public faVideo = faVideo;
+  public faShop = faShop;
+  public faPeopleGroup = faPeopleGroup;
+  public faConfig = faGear;
+  public faMessage = faMessage;
+  public faBell = faBell;
+  public faUser = faUser;
+  public faClose = faRightFromBracket;
+
 
   private router = inject(Router);
+  private authenticationService = inject(AuthenticationService);
 
   openHome() {
     this.router.navigateByUrl('/home');
+  }
+
+  logOut() {
+    this.authenticationService.logout();
+    this.router.navigateByUrl('');
   }
 
 }
