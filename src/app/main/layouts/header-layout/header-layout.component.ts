55d6f885037a0a6c9238127d6cbdaac2aefef4e9
Alexis <xavieralexisariashuapaya@hotmail.com>
2024-11-16 13:16:44 -0500

    feat: Header layout

--- src/app/main/layouts/header-layout/header-layout.component.ts
+++ src/app/main/layouts/header-layout/header-layout.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell, faGear, faHome, faMessage, faPeopleGroup, faShop, faUser, faUserGroup, faVideo } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header-layout',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header-layout.component.html',
   styleUrl: './header-layout.component.css'
 })
 export class HeaderLayoutComponent {
-
+  faHome = faHome;
+  faUserGroup = faUserGroup;
+  faVideo = faVideo;
+  faShop = faShop;
+  faPeopleGroup = faPeopleGroup;
+  faConfig = faGear;
+  faMessage = faMessage;
+  faBell = faBell;
+  faUser = faUser;
 }
