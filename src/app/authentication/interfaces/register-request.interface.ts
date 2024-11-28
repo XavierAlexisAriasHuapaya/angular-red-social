2a58a1331affabcf87c5a6743d825ef16cae65bf
Alexis <xavieralexisariashuapaya@hotmail.com>
2024-11-28 16:46:24 -0500

    fix: layout register

--- src/app/authentication/interfaces/register-request.interface.ts
+++ src/app/authentication/interfaces/register-request.interface.ts
@@ -1,4 +1,6 @@
 export interface registerRequest {
+    name: string;
+    lastname: string;
     username: string;
     password: string;
     email: string;
--- src/app/authentication/pages/register-page/register-page.component.ts
+++ src/app/authentication/pages/register-page/register-page.component.ts
@@ -5,10 +5,10 @@ import { registerRequest } from '../../interfaces/register-request.interface';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-register-page',
-    imports: [ReactiveFormsModule],
-    templateUrl: './register-page.component.html',
-    styleUrl: './register-page.component.css'
+  selector: 'app-register-page',
+  imports: [ReactiveFormsModule],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
 
@@ -18,14 +18,18 @@ export class RegisterPageComponent {
   private registerRequest!: registerRequest;
 
   public formRegister: FormGroup = this.formBuilder.group({
+    name: ['', Validators.required],
+    lastname: ['', Validators.required],
     username: ['', Validators.required],
     password: ['', Validators.required],
     email: ['', Validators.required]
   });
 
   public register() {
-    const { username, password, email } = this.formRegister.value;
+    const { name, lastname, username, password, email } = this.formRegister.value;
     this.registerRequest = {
+      name: name,
+      lastname: lastname,
       username: username,
       password: password,
       email: email
